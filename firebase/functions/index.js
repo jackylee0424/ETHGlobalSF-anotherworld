df6a23d01e743bc5569b115fbea6a9ea0d3e9f7d
Jackie Chia-Hsun Lee <jackylee0424@users.noreply.github.com>
2022-11-05 14:04:04 -0700

    add airdrop on goerli

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -13,10 +13,11 @@ const providerETHMainnet = new ethers.providers.JsonRpcProvider(
 );
 
 const providerGoerliTestnet = new ethers.providers.JsonRpcProvider(
-  `https://goerli.infura.io/v3/${INFURA_APIKEY}`
-  //`https://long-virulent-wish.ethereum-goerli.discover.quiknode.pro/${QUICKNODE_APIKEY}/`
+  //`https://goerli.infura.io/v3/${INFURA_APIKEY}`
+  `https://long-virulent-wish.ethereum-goerli.discover.quiknode.pro/${QUICKNODE_APIKEY}/`
 );
 
+// resolve ENS to address
 exports.resolveens = functions.https.onRequest((req, res) => {
   cors(req, res, async () => {
     const ens = req.query.ens;
@@ -46,6 +47,7 @@ exports.resolveens = functions.https.onRequest((req, res) => {
   });
 });
 
+// get token balance based on ETH mainnet ENS
 exports.gettokenbalance = functions.https.onRequest((req, res) => {
   cors(req, res, async () => {
     const ens = req.query.ens;
@@ -70,6 +72,50 @@ exports.gettokenbalance = functions.https.onRequest((req, res) => {
               network: "goerli",
               tokenId: tokenId,
               balance: Number(balance.toString()),
+              success: true,
+              ethAddress: ethAddress,
+              ens: ens,
+            });
+          } else {
+            res.status(200).send({ success: false, msg: 'invalid ENS' });
+          }
+        } catch (error) {
+          res.status(200).send({ success: false, msg: 'oops' });
+        }
+        break;
+      default:
+        res.status(405).json({ success: false, reason: 'Unsupported request method' });
+    }
+  });
+});
+
+// based on a ETH mainnet ENS, airdrop erc1155 tokens on goerli
+exports.airdrop = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const ens = req.query.ens;
+    switch (req.method) {
+      case 'GET': // handle GET request
+        try {                                                  
+          let ethAddress = null;
+          if (ens.includes(".eth") || ens.includes(".id") || ens.includes(".xyz")) {
+              ethAddress = await providerETHMainnet.resolveName(ens);
+          }
+          if (ethAddress) {
+            const signer = new ethers.Wallet(functions.config().operator.pkey, providerGoerliTestnet);
+            const vaultContract = new ethers.Contract(
+              vaultContractGoerli,
+              AnotherWorldVaultABI,
+              signer
+            );
+            const tokenId = 0; // TODO: expose to game
+            const amount = 1; // TODO: expose to game
+            const tx = await vaultContract.mint(ethAddress, tokenId, amount);
+
+            res.status(200).send({
+              network: "goerli",
+              tx: tx,
+              tokenId: tokenId,
+              amount: amount,
               success: true,
               ethAddress: ethAddress,
               ens: ens,
